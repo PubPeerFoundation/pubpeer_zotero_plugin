802104745d6b17412ed0d334e326866ee071f165
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-10-09 00:07:31 +0200

    strip bootstrap

--- bootstrap.ts
+++ bootstrap.ts
@@ -3,34 +3,16 @@ declare const Zotero: any
 import { log } from './content/log'
 
 export async function startup({ id, version, rootURI }) {
-	log("Starting 2.0");
-	
-	Zotero.PreferencePanes.register({
-		pluginID: 'make-it-red@example.com',
-		src: rootURI + 'preferences.xhtml',
-		scripts: [rootURI + 'preferences.js']
-	});
-	
-	Services.scriptloader.loadSubScript(rootURI + 'make-it-red.js');
-	MakeItRed.init({ id, version, rootURI });
-	MakeItRed.addToAllWindows();
-	await MakeItRed.main();
 }
 
 export function onMainWindowLoad({ window }) {
-	MakeItRed.addToWindow(window);
 }
 
 export function onMainWindowUnload({ window }) {
-	MakeItRed.removeFromWindow(window);
 }
 
 export function shutdown() {
-	log("Shutting down 2.0");
-	MakeItRed.removeFromAllWindows();
-	MakeItRed = undefined;
 }
 
 export function uninstall() {
-	log("Uninstalled 2.0");
 }
