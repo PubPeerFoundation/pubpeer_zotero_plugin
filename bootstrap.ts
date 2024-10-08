4d809626047d15f735e67fa840b8ff243e8fbf73
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-10-09 00:08:54 +0200

    strip bootstrap

--- bootstrap.ts
+++ bootstrap.ts
@@ -3,6 +3,7 @@ declare const Zotero: any
 import { log } from './content/log'
 
 export async function startup({ id, version, rootURI }) {
+  log('startup')
 }
 
 export function onMainWindowLoad({ window }) {
