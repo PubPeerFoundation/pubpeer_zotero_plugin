8f80d75e6123dad0a0355ff28a20001e4950aa07
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-11-12 11:29:05 +0100

    flash instead of alert

--- content/pubpeer.ts
+++ content/pubpeer.ts
@@ -7,9 +7,11 @@ import { flash } from './flash'
 import { localize } from './l10n'
 
 
+/*
 function alert({ title, text }: { title?: string; text: string }): void {
   Services.prompt.alert(null, title || 'Alert', text)
 }
+*/
 
 export function prompt({ title, text, value }: { title?: string; text: string; value?: string }): string {
   const wrap = { value: value || '' }
